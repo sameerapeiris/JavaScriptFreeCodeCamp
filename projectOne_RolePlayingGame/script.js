6f1a115b208f5a82f46b5462a887292648e4b717
sameerapeiris <sameedsdp@gmail.com>
2024-01-19 15:29:05 +0200

    update with script

---
+++ projectOne_RolePlayingGame/script.js
@@ -0,0 +1,5 @@
+//JavaScript it is best practice to use the let keyword instead. This fixes several unusual behaviors with var that can make your code difficult to debug.
+let  xp = 0;
+let  health = 100;
+let  gold = 50;
+let  currentWeapon = 0;
